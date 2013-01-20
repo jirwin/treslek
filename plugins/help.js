445b4e87688180cd9be4951dc69a6bbf58713467
Justin Gallardo <justin.gallardo@gmail.com>
2013-01-20 07:50:11 -0800

    Add help plugin.

---
+++ plugins/help.js
@@ -0,0 +1,32 @@
+/*
+ * Help plugin
+ *  - help: Displays available commands and their usage
+ */
+var Help = function() {
+  this.commands = ['help'];
+};
+
+
+/*
+ * Help command. With no options it displays available commands. If you
+ * specify a command, it will out put the usage info if available.
+ */
+Help.prototype.help = function(bot, to, from, msg, callback) {
+  var response;
+
+  if (msg === '') {
+    response = 'Available commands: ' + bot.commands.join(', ');
+  } else if (bot.usage.hasOwnProperty(msg) && bot.usage[msg] === '') {
+    response = "I don't have help information for " + msg;
+  } else if (bot.usage.hasOwnProperty(msg)) {
+    response = bot.usage[msg];
+  } else {
+    response = "I don't know about the command: " + msg;
+  }
+
+  bot.say(to, response);
+  callback();
+};
+
+
+exports.Plugin = Help;
