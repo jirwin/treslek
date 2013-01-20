4b220bbc43ab22b0009084a5add5272ab7572694
Justin Gallardo <justin.gallardo@gmail.com>
2013-01-20 04:52:50 -0800

    Remove extra hook from echo plugin.

--- plugins/echo.js
+++ plugins/echo.js
@@ -1,16 +1,11 @@
 var Plugin = function() {
   this.commands = ['echo'];
-  this.hooks = ['hook'];
+  this.hooks = [];
 };
 
 Plugin.prototype.echo = function(bot, to, from, msg, callback) {
   bot.say(to, msg);
   callback();
 };
-
-Plugin.prototype.hook = function(bot, to, from, msg, callback) {
-  console.log("Echo hook");
-  callback();
-}
 
 exports.Plugin = Plugin;
