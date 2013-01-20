6d18357ab416b55456c9678dec0cbc948a17b6a6
Justin Gallardo <justin.gallardo@gmail.com>
2013-01-20 06:31:17 -0800

    Add isup.me plugin.

---
+++ plugins/isup.js
@@ -0,0 +1,50 @@
+var request = require('request');
+
+
+/*
+ * Isup plugin
+ *   - isup: Checks isup.me to see if a website is up or down
+ */
+var Isup = function() {
+  this.commands = ['isup'];
+};
+
+
+/*
+ * Isup command
+ */
+Isup.prototype.isup = function(bot, to, from, msg, callback) {
+  var url, response;
+
+  if (msg === '') {
+    bot.say(to, 'I need a URL to check');
+    callback();
+    return;
+  }
+
+  url = msg.replace(/^http:\/\//, '');
+
+  request('http://isup.me/' + url, function (error, response, body) {
+    if (error || response.statusCode !== 200) {
+      bot.say(to, 'Something is wrong with isup.me.');
+      callback();
+      return;
+    }
+
+    if (!error && response.statusCode === 200) {
+      if (body.search('is up') !== -1) {
+        response = "Hooray! " + msg + ' looks good to me.';
+      } else if (body.search('Huh') !== -1) {
+        response = "Har Har. " + msg + " doesn't look like a website to me.";
+      } else if (body.search('down from here') !== -1) {
+        response = "Oh no! " + msg + " is broken.";
+      }
+      
+      bot.say(to, response);
+      callback();
+    }
+  })
+};
+
+
+exports.Plugin = Isup;
