3e921cda1243fb273a559f4de7c569248f280238
Kevin Purdy <kpurdy@criticalpath.com>
2013-01-23 21:51:01 -0800

    Resolved conflicts with jirwin's code style. Less stupid javascript. more dots

--- plugins/spotify.js
+++ plugins/spotify.js
@@ -1,6 +1,7 @@
 var async = require('async');
 var request = require('request');
-var spotibase = 'http://ws.spotify.com/lookup/1/.json?uri='
+var sprintf = require('sprintf').sprintf;
+
 /*
  * Spotify Plugin
  *   - creates a hook that fetches information from spotify urls
@@ -12,8 +13,9 @@ var Spotify = function() {
 };
 
 Spotify.prototype.spotify = function(bot, to, from, msg, callback) {
+  var spotibase = 'http://ws.spotify.com/lookup/1/.json?uri=';
   var spotifyReg = /spotify:[a-z0-9:]+/gi, 
-    matches = msg.match(spotifyReg);
+  matches = msg.match(spotifyReg);
   
   if (!matches) {
       callback();
@@ -25,36 +27,32 @@ Spotify.prototype.spotify = function(bot, to, from, msg, callback) {
       var response, data;
       
       if (err || res.statusCode === 404) {
-	console.log('Error pulling from Spotify');
-	callback();
-	return;
+        console.log('Error pulling from Spotify');
+        callback();
+        return;
       }
 
       if (res.statusCode === 200) {
-	data = JSON.parse(body);
-	if (data['info']['type'] == 'track') {
-	    var artistNames = [];
-	    data['track']['artists'].forEach(function(art) {
-		    artistNames.push(art['name']);
-		});
-	    response = artistNames.join(', ')
-	      +' - '
-	      + data['track']['name']
-	      + ' ['
-	      + data['track']['album']['name']
-	      + ' ('
-	      + data['track']['album']['released']
-	      + ')]';
+        data = JSON.parse(body);
+        if (data.info.type === 'track') {
+          var artistNames = [];
+          data.track.artists.forEach(function(art) {
+            artistNames.push(art.name);
+          });
+          response = sprintf('%s - %s [%s (%s)]', 
+	    artistNames.join(', '),
+	    data.track.name,
+	    data.track.album.name,
+            data.track.album.released);
 	}
-	else if (data['info']['type'] == 'album') {
-	  response = data['album']['artist']
-	    + "'s "
-	    + data['album']['name']
-	    + ' ('
-	    + data['album']['released'] + ')';
+	else if (data.info.type === 'album') {
+	  response = sprintf("%s's %s (%s)", 
+	    data.album.artist,
+	    data.album.name,
+	    data.album.released);
 	}
-	else if (data['info']['type'] == 'artist') {
-	    response = data['artist']['name'];
+	else if (data.info.type === 'artist') {
+	  response = data.artist.name;
 	}
 
 	bot.say(to, response);
