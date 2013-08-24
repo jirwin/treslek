1c5a7d13edd4b019eea7700b7ecb4011a8ca2d85
Justin Gallardo <justin.gallardo@gmail.com>
2013-08-24 00:38:59 +0000

    FIXED IT

--- plugins/github.js
+++ plugins/github.js
@@ -1,5 +1,5 @@
 var redis = require('redis');
-var qs = require('querystring');
+var qs = require('qs');
 var sprintf = require('sprintf').sprintf;
 
 
@@ -17,9 +17,18 @@ Github.prototype.listen = function(bot) {
   redisClient.on('pmessage', function(pattern, channel, message) {
     var realChannel = channel.slice(bot.redisConf.prefix.length - 1).split('/'),
         ircChannel = bot.config.github.channels[realChannel[1]],
-        payload = JSON.parse(qs.parse(message).payload);
+        payload = JSON.parse(qs.parse(message).payload),
+        msg = "I got something from github.";
 
-    bot.say(ircChannel, '');
+    if (payload.action === 'opened') {
+      msg = sprintf('%s opened a new PR at %s', payload.sender.login, payload.pull_request.url);
+    }
+
+    if (payload.action === 'reopened') {
+      msg = sprintf('%s reopened a new PR at %s', payload.sender.login, payload.pull_request.url);
+    }
+
+    bot.say(ircChannel, msg);
   });
 };
 
