96c4544314b7328acfd4c7ca4c5492fc184a7c58
Justin Gallardo <justin.gallardo@rackspace.com>
2013-10-06 21:56:57 -0700

    initial maas integration step.

---
+++ plugins/maas.js
@@ -0,0 +1,21 @@
+var redis = require('redis');
+
+
+var Github = function() {
+  this.auto = ['listen'];
+};
+
+Github.prototype.listen = function(bot) {
+  var redisClient = redis.createClient(bot.redisConf.port, bot.redisConf.host),
+      pattern = [bot.redisConf.prefix, 'github/*'].join(':');
+
+  redisClient.on('message', function(channel, message) {
+    var realChannel = channel.slice(bot.redisConf.prefix.length - 1).split('/'),
+        ircChannel = bot.config.github[realChannel];
+
+    bot.say(realChannel, 'I got a message from github.');
+  });
+  redisClient.psubscribe(pattern);
+};
+
+exports.Plugin = Github;
