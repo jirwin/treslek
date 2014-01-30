3fc3bc7fa7f473ae84b7d7a597c88533c2c8a792
Kevin Purdy <kevin@project529.com>
2014-01-29 18:12:11 -0800

    Fixed whitespace.

--- plugins/doge.js
+++ plugins/doge.js
@@ -6,36 +6,36 @@ var sprintf = require('sprintf').sprintf;
  * doge/btc price
  */
 
-var DOGE = function() {
-  this.commands = ['doge'];
-  this.usage = {
-      doge: 'ex : !doge . Gets the current cryptsy price of DOGE'
-  };
+var DOGE = function () {
+    this.commands = ['doge'];
+    this.usage = {
+        doge: 'ex : !doge . Gets the current cryptsy price of DOGE'
+    };
 };
 
 /*
  * primary doge command
  */
 
-DOGE.prototype.doge = function(bot, to, from, msg, callback) {
-  var msgOut = '';
-  url = 'http://pubapi.cryptsy.com/api.php?method=singlemarketdata&marketid=132';
-  request(url, function(err, res, body) {
-      if (!body) {
-	  bot.say(to, 'Unable to retreive DOGE price');
-          callback();
-          return;
-      }
-      
-      data = JSON.parse(body);
-      msgOut = sprintf(
-        'Cryptsy price: %s %s',
-	data.return.markets.DOGE.lasttradeprice,
-	data.return.markets.DOGE.label
-      );
+DOGE.prototype.doge = function (bot, to, from, msg, callback) {
+    var msgOut = '';
+    url = 'http://pubapi.cryptsy.com/api.php?method=singlemarketdata&marketid=132';
+    request(url, function (err, res, body) {
+        if (!body) {
+            bot.say(to, 'Unable to retreive DOGE price');
+            callback();
+            return;
+        }
 
-      bot.say(to, msgOut);
-      callback();
+        data = JSON.parse(body);
+        msgOut = sprintf(
+            'Cryptsy price: %s %s',
+            data.return.markets.DOGE.lasttradeprice,
+            data.return.markets.DOGE.label
+        );
+
+        bot.say(to, msgOut);
+        callback();
     });
 };
 
