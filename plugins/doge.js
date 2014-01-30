9d7cc341548b0cfa8de43f80c8fd374cb682e093
Kevin Purdy <kevin@project529.com>
2014-01-30 13:20:13 -0800

    Doge fetch all at once! Many exchange. Such money.

--- plugins/doge.js
+++ plugins/doge.js
@@ -1,5 +1,6 @@
 var request = require('request');
 var sprintf = require('sprintf').sprintf;
+var async = require('async');
 
 /*
  * DOGE Plugin, queries cryptsy for current 
@@ -9,8 +10,87 @@ var sprintf = require('sprintf').sprintf;
 var DOGE = function () {
     this.commands = ['doge'];
     this.usage = {
-        doge: 'ex : !doge . Gets the current cryptsy price of DOGE'
+        doge: 'ex : !doge . Gets market prices for DOGE'
     };
+};
+
+
+
+/* Vicurex 1 / value
+ * https://api.vircurex.com/api/get_last_trade.json?base=BTC&alt=DOGE
+ *  1 / value
+ */
+var VICUREX = function (callback) {
+    var url = 'https://api.vircurex.com/api/get_last_trade.json?base=BTC&alt=DOGE';
+    var label = 'Vicurex';
+    var value;
+
+    request(url, function (err, res, body) {
+        if (!body) {
+            value = 'such no body';
+        } else {
+            try {
+                data = JSON.parse(body);
+                value = (1 / data.value).toPrecision(3);
+            } catch (err) {
+                value = 'Response very confuse';
+            }
+        }
+        callback(null, [label, value]);
+    });
+};
+
+/* Coinex
+ * https://coinex.pw/api/v2/trade_pairs
+ * need pair 46
+ * last_price / 100000000
+ */
+var COINEX = function (callback) {
+    var url = 'https://coinex.pw/api/v2/trade_pairs';
+    var label = 'Coinex';
+    var value;
+
+    request(url, function (err, res, body) {
+        if (!body) {
+            value = 'such no body';
+        } else {
+            try {
+                data = JSON.parse(body);
+                data.trade_pairs.forEach(function (each) {
+                    if (each.id == 46) {
+                        value = each.last_price / 100000000;
+                    }
+                });
+            } catch (err) {
+                value = 'Response very confuse';
+            }
+        }
+        callback(null, [label, value]);
+    });
+};
+
+/* Cryptsy
+ * http://pubapi.cryptsy.com/api.php?method=singlemarketdata&marketid=132
+ *
+ */
+var CRYPTSY = function (callback) {
+    var url = 'http://pubapi.cryptsy.com/api.php?method=singlemarketdata&marketid=132';
+    var label = 'Cryptsy';
+    var value;
+
+    request(url, function (err, res, body) {
+        if (!body) {
+            value = 'such no body';
+        } else {
+            try {
+                data = JSON.parse(body);
+                value = data.return.markets.DOGE.lasttradeprice;
+            } catch (err) {
+                value = 'Response very confuse';
+            }
+        }
+        callback(null, [label, value]);
+    });
 };
 
 /*
@@ -19,27 +99,14 @@ var DOGE = function () {
 
 DOGE.prototype.doge = function (bot, to, from, msg, callback) {
     var msgOut = '';
-    url = 'http://pubapi.cryptsy.com/api.php?method=singlemarketdata&marketid=132';
-    request(url, function (err, res, body) {
-        if (!body) {
-            bot.say(to, 'Unable to retrieve DOGE price');
-            callback();
-            return;
-        }
+    async.parallel([CRYPTSY, COINEX, VICUREX], function (err, results) {
+       msgOut += "DOGE: ";
+       results.forEach(function (each) {
+           msgOut += each[0] + ": " + each[1] + " "
+       });
 
-        try {
-            data = JSON.parse(body);
-            msgOut = sprintf(
-                'Cryptsy price: %s %s',
-                data.return.markets.DOGE.lasttradeprice,
-                data.return.markets.DOGE.label
-            );
-        } catch (err) {
-            msgOut = 'Response very confuse';
-        }
-
-        bot.say(to, msgOut);
-        callback();
+       bot.say(to, msgOut);
+       callback();
     });
 };
 
