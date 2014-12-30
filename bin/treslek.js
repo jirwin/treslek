7ab34dcb26c542ed07c5cfbef2c733a24f8db5da
Justin Gallardo <justin.gallardo@gmail.com>
2014-12-30 12:55:21 -0800

    Support custom config file locations.

--- bin/treslek.js
+++ bin/treslek.js
@@ -1,9 +1,10 @@
 #!/usr/bin/env node
 
 var logmagic = require('logmagic');
+var argv = require('minimist')(process.argv.slice(2));
 
 var treslek = require('../lib/treslek');
-var conf = require('../conf.json');
+var conf = require(argv._[0] || '../conf.json');
 
 logmagic.registerSink('stdout', function(module, level, message) { console.log(message); });
 logmagic.route('__root__', logmagic.INFO, 'stdout');
