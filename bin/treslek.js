8639dc00033b4a7bae59243e1126a307abeb1bcf
Justin Gallardo <justin.gallardo@gmail.com>
2014-12-30 16:47:19 -0800

    Clean up logging a bit.

--- bin/treslek.js
+++ bin/treslek.js
@@ -6,7 +6,10 @@ var argv = require('minimist')(process.argv.slice(2));
 var treslek = require('../lib/treslek');
 var conf = require(argv._[0] || '../conf.json');
 
-logmagic.registerSink('stdout', function(module, level, message) { console.log(message); });
+logmagic.registerSink('stdout', function(module, level, message, extra) {
+  var now = new Date();
+  console.log(now.toISOString() + ' (' + now.getTime() + '):', message, JSON.stringify(extra));
+});
 logmagic.route('__root__', logmagic.INFO, 'stdout');
 
 var bot = new treslek.TreslekBot(conf);
