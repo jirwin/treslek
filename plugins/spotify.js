265bfbd507102ea998ac2411721bb49d78ec82eb
Kevin Purdy <kevin@ridewithgps.com>
2016-02-01 15:20:47 -0800

    Updated spotify plugin to new api endpoint and format. Also rewrote and extended interface to allow for better options later.

--- plugins/spotify.js
+++ plugins/spotify.js
@@ -9,65 +9,87 @@ var log = require('logmagic').local('treslek.plugins.spotify');
  *   - creates a hook that fetches information from spotify urls
  *
  */
-var Spotify = function() {
+var Spotify = function () {
   this.hooks = ['spotify'];
+};
+
+var ENDPOINTS = {
+  artist: {
+    url: "artists/",
+    response: function (data) {
+      return data.name;
+    }
+  },
+  album: {
+    url: "albums/",
+    response: function (data) {
+      var arts = data.artists.map(function (bit) {
+        return bit.name;
+      }).join(", ");
+      return sprintf(
+        '%s - %s (%s)',
+        data.name,
+        arts,
+        data.release_date
+      );
+    }
+  },
+  track: {
+    url: "tracks/",
+    response: function (data) {
+      var arts = data.artists.map(function (bit) {
+        return bit.name;
+      }).join(", ");
+      return sprintf(
+        '%s - %s [%s]',
+        arts,
+        data.name,
+        data.album.name
+      );
+    }
+  }
 };
 
 /*
  * spotify hook.
  */
-Spotify.prototype.spotify = function(bot, to, from, msg, callback) {
-  var spotibase = 'http://ws.spotify.com/lookup/1/.json?uri=',
-      spotifyReg = /spotify:[a-z0-9:]+/gi,
-      matches = msg.match(spotifyReg);
+Spotify.prototype.spotify = function (bot, to, from, msg, callback) {
+  var spotibase = 'https://api.spotify.com/v1/',
+    spotifyReg = /spotify:[a-z0-9:]+/gi,
+    matches = msg.match(spotifyReg);
 
   if (!matches) {
     callback();
     return;
   }
 
-  async.forEach(matches, function(url, callback) {
-    request(spotibase + url, function(err, res, body) {
-      var response, data, artistNames;
+  async.forEach(matches, function (match, callback) {
 
-      if (err || res.statusCode === 404) {
-        log.error('Error pulling from spotify', {err: err, statusCode: res.statusCode});
-        callback();
-        return;
-      }
+    var bits = match.split(":"),
+      type = bits[1],
+      id = bits[2],
+      hook = ENDPOINTS[type];
 
-      if (res.statusCode === 200) {
-        data = JSON.parse(body);
+    if (hook) {
 
-        if (data.info.type === 'track') {
-          artistNames = [];
-          data.track.artists.forEach(function(art) {
-            artistNames.push(art.name);
-          });
-          response = sprintf(
-            '%s - %s [%s (%s)]',
-            artistNames.join(', '),
-            data.track.name,
-            data.track.album.name,
-            data.track.album.released
-          );
-        } else if (data.info.type === 'album') {
-          response = sprintf(
-            "%s's %s (%s)",
-            data.album.artist,
-            data.album.name,
-            data.album.released
-          );
-        } else if (data.info.type === 'artist') {
-          response = data.artist.name;
+      request(spotibase + hook.url + id, function (err, res, body) {
+
+        if (err || res.statusCode === 404) {
+          log.error('Error pulling from spotify', {err: err, statusCode: res.statusCode});
+          callback();
+          return;
         }
 
-        bot.say(to, response);
-        callback();
-      }
-    });
+        if (res.statusCode === 200) {
+          var data = JSON.parse(body);
+          bot.say(to, hook.response(data));
+          callback();
+        }
+      });
+    } else {
+      callback();
+    }
   }, callback);
 };
-
 
 exports.Plugin = Spotify;
